27d806d50a5bb62179a5b11de25208e45156ddcd
Danilo Fróes <danilodavifroes@gmail.com>
2024-06-10 00:59:51 -0300

    Código original do último comitt

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,172 @@
-#include <Arduino.h>
+/* Olá, este é um projeto de um sistema com arduino para presentear pessoas que você ame.
 
-// put function declarations here:
-int myFunction(int, int);
+Nome do Projeto: AmorTron
+Nome do programador: Danilo Davi Gomes Fróes
+@dgfroes
+
+Código do AmorTron: */
+
+#include <Servo.h>
+#include <Arduino.h>
+#include <LiquidCrystal.h>
+ 
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2); //Define os pinos que serão utilizados para ligação ao display
+Servo meuServo; // Cria um objeto de servo
+bool carregando = true;
+int pos;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  meuServo.attach(9); // Define o pino ao qual o servo está conectado
+  meuServo.write(0);
+  lcd.begin(16, 2); //Define o tamanho do LCD (16 colunas e 2 linhas)
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+    
+    //Fazer o servo girar 
+    for (int pos = 0; pos <= 180; pos++) {
+    meuServo.write(pos);
+    delay(10); //Velocidade do servo
+    }
+    for (int pos = 180; pos >= 0; pos--) {
+    meuServo.write(pos);
+    delay(10); //Velocidade do servo
+    }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+      //Mensagem de carregando ao iniciar
+    if (carregando) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Carregando...");
+    delay(5000);
+
+    carregando = false;
+      
+    }
+
+      //Loop das outras mensagens 
+    else {
+        
+    lcd.clear();
+    lcd.setCursor(3,0);
+    lcd.print("Ola, minha");
+    lcd.setCursor(4,1);
+    lcd.print("princesa");
+    delay(4000);
+
+    //Fazer o servo girar 
+    for (int pos = 0; pos <= 180; pos++) {
+    meuServo.write(pos);
+    delay(10); //Velocidade do servo
+    }
+    for (int pos = 180; pos >= 0; pos--) {
+    meuServo.write(pos);
+    delay(10); //Velocidade do servo
+    }
+      
+    lcd.clear();
+    lcd.setCursor(5, 0);
+    lcd.print("Feliz");
+    lcd.setCursor(2,1);
+    lcd.print("aniversario!");
+    delay(5000);
+
+    //Fazer o servo girar 
+    for (int pos = 0; pos <= 180; pos++) {
+    meuServo.write(pos);
+    delay(10); //Velocidade do servo
+    }
+    for (int pos = 180; pos >= 0; pos--) {
+    meuServo.write(pos);
+    delay(10); //Velocidade do servo
+    }
+      
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Lembre sempre q");
+    lcd.setCursor(0,1);
+    lcd.print("eu te amo mt <3");
+    delay(5000);
+
+    //Fazer o servo girar 
+    for (int pos = 0; pos <= 180; pos++) {
+    meuServo.write(pos);
+    delay(10); //Velocidade do servo
+    }
+    for (int pos = 180; pos >= 0; pos--) {
+    meuServo.write(pos);
+    delay(10); //Velocidade do servo
+    }
+
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Voce eh o amor");
+    lcd.setCursor(0,1);
+    lcd.print("da minha vida");
+    delay(5000);
+
+    //Fazer o servo girar 
+    for (int pos = 0; pos <= 180; pos++) {
+    meuServo.write(pos);
+    delay(10); //Velocidade do servo
+    }
+    for (int pos = 180; pos >= 0; pos--) {
+    meuServo.write(pos);
+    delay(10); //Velocidade do servo
+    }
+
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Tenho muito");
+    lcd.setCursor(0,1);
+    lcd.print("orgulho de voce");
+    delay(5000);
+
+    //Fazer o servo girar 
+    for (int pos = 0; pos <= 180; pos++) {
+    meuServo.write(pos);
+    delay(10); //Velocidade do servo
+    }
+    for (int pos = 180; pos >= 0; pos--) {
+    meuServo.write(pos);
+    delay(10); //Velocidade do servo
+    }
+
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("cause i love");
+    lcd.setCursor(0,1);
+    lcd.print("u so much");
+    delay(5000);
+
+    //Fazer o servo girar 
+    for (int pos = 0; pos <= 180; pos++) {
+    meuServo.write(pos);
+    delay(10); //Velocidade do servo
+    }
+    for (int pos = 180; pos >= 0; pos--) {
+    meuServo.write(pos);
+    delay(10); //Velocidade do servo
+    }
+
+    lcd.clear();
+    lcd.setCursor(6,0);
+    lcd.print("L<3D");
+    lcd.setCursor(5,1);
+    lcd.print("4ever");
+    delay(5000);
+
+    //Fazer o servo girar 
+    for (int pos = 0; pos <= 180; pos++) {
+    meuServo.write(pos);
+    delay(10); //Velocidade do servo
+    }
+    for (int pos = 180; pos >= 0; pos--) {
+    meuServo.write(pos);
+    delay(10); //Velocidade do servo
+    }
+
+    }
+
+    }
